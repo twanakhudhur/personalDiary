6a82503d66b4c887d86d865420fc2f7fb08ef6e2
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-09-03 16:21:11 +0200

    created jsx files + some initial outline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +9,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+
+  daisyui: {
+    themes: ["autumn", "dark"],
+  },
+  plugins: [daisyui],
 }
 
