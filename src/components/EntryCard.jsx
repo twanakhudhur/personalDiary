e46a43a4be4d1123f9801f24d8980bbc6c153ca4
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-09-05 00:50:36 +0200

    open details modal fix

--- src/components/EntryCard.jsx
+++ src/components/EntryCard.jsx
@@ -6,10 +6,15 @@ function EntryCard({ entry }) {
   const { date, title, url } = entry;
   const [open, setOpen] = useState(false);
 
-
   const closeModal = () => {
     setOpen(false);
     console.log("close modal clicked");
+    console.log(open);
+  };
+
+  const openModal = () => {
+    setOpen(true);
+    console.log("open modal clicked");
     console.log(open);
   };
 
@@ -19,27 +24,28 @@ function EntryCard({ entry }) {
   // };
 
   return (
-    <div
-      onClick={() => setOpen(!open)}
-      className="flex flex-row card card-compact bg-primary w-full shadow-xl p-2 hover:bg-accent cursor-pointer"
-    >
-      <figure className="w-36 h-28 rounded-lg">
-        <img
-          src={url}
-          alt={`entry image for ${date}`}
-          className="object-scale-down overflow-hidden rounded-lg"
-        />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{date}</h2>
-        <p>{title}</p>
+    <div>
+      <div
+        onClick={() => openModal()}
+        className="flex flex-row card card-compact bg-primary w-full shadow-xl p-2 hover:bg-accent cursor-pointer"
+      >
+        <figure className="w-36 h-28 rounded-lg">
+          <img
+            src={url}
+            alt={`entry image for ${date}`}
+            className="object-scale-down overflow-hidden rounded-lg"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{date}</h2>
+          <p>{title}</p>
+        </div>
       </div>
       {open &&
         createPortal(
           <EntryDetails closeModal={closeModal} entry={entry} />,
           document.body
         )}
-
     </div>
   );
 }
